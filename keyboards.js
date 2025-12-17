7eadfa4805d9a3617b97a93317cb868ef1131a4e
abdulgafurovrv-design <abdulgafurov.r.v@gmail.com>
2025-12-17 13:01:12 +0300

    Create keyboards.js

---
+++ keyboards.js
@@ -0,0 +1,6 @@
+const { Markup } = require('telegraf');
+
+function mainKeyboard() { /* ... */ }
+function cancelLastKeyboard() { /* ... */ }
+
+module.exports = { mainKeyboard, cancelLastKeyboard };
