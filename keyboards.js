13caa52f2363933ec6ae66d21512d8055a2baa24
abdulgafurovrv-design <abdulgafurov.r.v@gmail.com>
2025-12-17 15:25:32 +0300

    Update keyboards.js

--- keyboards.js
+++ keyboards.js
@@ -2,22 +2,16 @@ const { Markup } = require('telegraf');
 
 function mainKeyboard() {
   return Markup.inlineKeyboard([
-    [Markup.button.callback("Баланс", "balance"), Markup.button.callback("Отчёт", "report")],
-    [Markup.button.callback("Должники", "debtors"), Markup.button.callback("Перевод", "transfer")],
-    [Markup.button.callback("Расход +", "expense"), Markup.button.callback("Доход +", "income")]
-  ]);
-}
-
-function cancelLastKeyboard() {
-  return Markup.inlineKeyboard([
-    [Markup.button.callback("Отменить последнюю", "cancel_last")]
+    [Markup.button.callback('Баланс', 'balance'), Markup.button.callback('Отчёт', 'report')],
+    [Markup.button.callback('Должники', 'debtors'), Markup.button.callback('Перевод', 'transfer')],
+    [Markup.button.callback('Расход +', 'expense'), Markup.button.callback('Доход +', 'income')]
   ]);
 }
 
 function menuKeyboard() {
   return Markup.inlineKeyboard([
-    [Markup.button.callback("Меню", "menu")]
+    [Markup.button.callback('Меню', 'menu')]
   ]);
 }
 
-module.exports = { mainKeyboard, cancelLastKeyboard, menuKeyboard };
+module.exports = { mainKeyboard, menuKeyboard };
