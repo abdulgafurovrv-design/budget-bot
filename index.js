3600e5594ceaeabd62b0680479b4cb9d6df179bd
abdulgafurovrv-design <abdulgafurov.r.v@gmail.com>
2025-12-17 13:05:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const { Telegraf } = require('telegraf');
-const { initDoc, initSheets } = require('./sheets');
-const { handleText } = require('./handlers');
-const { mainKeyboard } = require('./keyboards');
 
 const TOKEN = '8269910739:AAEywu7dOX8WB9TDG6y8WH-fAoV5_foRhzU';
 
 const bot = new Telegraf(TOKEN);
 
-initDoc().catch(console.error);
-initSheets();
+require('./sheets'); // инициализация таблиц
+const { handleText, handleCallback } = require('./handlers');
+const { mainKeyboard } = require('./keyboards');
 
+// Обработка сообщений
 bot.on('text', handleText);
 
+// Обработка нажатий на кнопки
+bot.on('callback_query', handleCallback);
+
 bot.launch();
-console.log('Бот запущен!');
+console.log('Бот запущен на Telegraf + Render!');
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
